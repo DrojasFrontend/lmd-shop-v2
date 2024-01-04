80035c64d4c3c723e0c52fa9a5011bddaa5fb90b
Daniel Rojas <danielrojas243@gmail.com>
2024-01-04 14:17:29 -0500

    feat: Add styles mobile

--- functions.php
+++ functions.php
@@ -82,4 +82,6 @@ function my_acf_json_save_point( $path ) {
 }
 add_filter( 'acf/settings/save_json', 'my_acf_json_save_point' );
 
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
 ?>
