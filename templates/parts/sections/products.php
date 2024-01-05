3893f8d108b723b90ce5420d0cf481c9485c27d7
Daniel Rojas <danielrojas243@gmail.com>
2024-01-04 22:17:01 -0500

    refactor: optimize image handling and code structure

--- templates/parts/sections/products.php
+++ templates/parts/sections/products.php
@@ -42,18 +42,18 @@ $posts = $query->get_posts();
             'alt'       => esc_attr($title),
             'width'     => $image_meta['width'],
             'height'    => $image_meta['height'],
-            'class'     => 'attachment-medium size-medium wp-post-image',
+            'class'     => 'attachment-medium size-medium wp-post-image daniel',
             'decoding'  => 'async',
             'fetchpriority' => 'high',
             'srcset'    => wp_get_attachment_image_srcset($thumbnail_id, 'medium'),
-            'sizes'     => '(max-width: ' . $image_meta['width'] . 'px) 100vw, ' . $image_meta['width'] . 'px',
+            'sizes'     => '(min-width: 1280px) 243px, (min-width: 1040px) calc(18.18vw + 14px), (min-width: 780px) 300px, calc(46.74vw - 55px)',
           )
         );
       ?>
       <li>
           <a href="<?= esc_url($link) ?>" target="_blank">
               <figure>
-                  <?= $thumbnail ?>
+              <?= $thumbnail ?>
               </figure>
               <p><?= esc_html($title) ?></p>
           </a>
